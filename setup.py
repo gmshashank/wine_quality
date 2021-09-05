c660506238066ba1a21d80290b6929cb87c9e968
Shashank Mewada <gmshashank@gmail.com>
2021-09-05 01:12:32 +0100

    Implemented pytest and tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Wine qualtiy classification package",
+    author="gmshashank",
+    packages=find_packages(),
+    license="MIT"
+)
